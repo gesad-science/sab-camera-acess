78697a2e4f74369d715e1742f48fc36d41951235
Guilherme Gaspar <guilhermevgaspar07@gmail.com>
2026-02-19 15:12:35 -0300

    change api endpoint to facial classification

--- app/src/main/java/api/ApiConfigManager.kt
+++ app/src/main/java/api/ApiConfigManager.kt
@@ -1,7 +1,7 @@
 package api
 
 object ApiConfigManager {
-    private const val BASE_URL = "https://nonpossibly-aspish-fletcher.ngrok-free.dev/mock/classify"
+    private const val BASE_URL = "https://nonpossibly-aspish-fletcher.ngrok-free.dev/classify"
     private const val MODEL_NAME = "mock"
 
     fun getFinalUrl(): String = BASE_URL
